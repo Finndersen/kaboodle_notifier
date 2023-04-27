f4d6cb3381f4fef348d53c9644fa011f728a8e63
Finn Andersen <finndersen@gmail.com>
2023-04-27 17:02:07 +0100

    My tweaks
    Updated to support multiple events, error messaging and improved checking logic

--- kabuto.py
+++ kabuto.py
@@ -4,38 +4,66 @@ import os
 from twilio.rest import Client
 from datetime import datetime
 
-account_sid = os.environ["TWILIO_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-twilio_number = os.environ["TWILIO_NUMBER"]
-my_number = os.environ["MY_NUMBER"]
+# Twilio Details (need to fill in)
+account_sid = ''
+auth_token = ''
+twilio_number = ''
+my_number = ''
+
+# Cookie data (need to fill in)
+cookies = {
+    '__stripe_mid': '',
+    '__stripe_sid': '',
+    'kabFLOW': ''
+}
+
+events = [
+    ['Event Name', 1234],
+]
 
 api_url = "https://bookings.kaboodle.co.uk/api/package/ticketavailability"
-api_headers = {'package-id': '9261',
-           'referer':'https://bookings.kaboodle.co.uk/book/9261/ticket',
-           'accept': 'application/json',
-           'cookie': os.environ["KABOODLE_COOKIE"]
-          }
+event_url_template = 'https://bookings.kaboodle.co.uk/book/{}/ticket'
 
-def count_tickets():
-    apiurl = api_url
-    headers = api_headers
-    response = requests.get(apiurl, allow_redirects=True, headers=headers)
-    print("\n" + str(datetime.now()) + "\n")
-    print(str(response.json()) + "\n")
+def check_tickets():
+    
+    for event_name, event_id in events:
+        print('Checking event: {}'.format(event_name))
+        event_url = event_url_template.format(event_id)
 
-    for item in response.json()['tickets']:
-        if item['@id'] == 5324:
-            tickets_available = item['@available']
-            print("There are " + str(tickets_available) + " tickets available")
-            if tickets_available > 0:
-                client = Client(account_sid, auth_token)
-                message = client.messages.create(
-                    body = "ticket available! https://queue.kaboodle.co.uk/?c=kaboodle&e=kaboodlequeue&t_client_id=39&t_agent_id=408740&t_package_id=9261",
-                    from_= twilio_number,
-                    to = my_number
-                )
-                print(message.sid)
+        api_headers = {'package-id': str(event_id),
+            'referer': event_url,
+            'accept': 'application/json',
+            # 'cookie': cookies
+            }
+        response = requests.get(api_url, allow_redirects=True, headers=api_headers, cookies=cookies)
+        print("\n" + str(datetime.now()) + "\n")
+        print(str(response.json()) + "\n")
+        if response.status_code < 300:
+            for item in response.json()['tickets']:
+                tickets_available = item['available']
+                if tickets_available and not item['sold_out']:
+                    print("There are " + str(tickets_available) + " tickets available for {}!".format(event_name))
+                    client = Client(account_sid, auth_token)
+                    message = client.messages.create(
+                        body = "{} tickets available for {}! {}".format(
+                            tickets_available,
+                            event_name,
+                            event_url
+                        ),
+                        from_= twilio_number,
+                        to = my_number
+                    )
+                    break
+        else:
+            print('REQUEST ERROR')
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                body = "Error with API request: {}".format(
+                    response.json()
+                ),
+                from_= twilio_number,
+                to = my_number
+            )
+            break
 
-while True:
-  count_tickets()
-  time.sleep(300)
+check_tickets() 
